c886337c1b82bb602ad6cb8a7dd2aa26e1cb2628
ss <helensunlife@gmail.com>
2019-08-29 00:22:03 -0700

    finished selenium tests; packaged.

--- src/test/java/edu/udacity/java/nano/WebSocketChatApplicationTest.java
+++ src/test/java/edu/udacity/java/nano/WebSocketChatApplicationTest.java
@@ -3,11 +3,16 @@ package edu.udacity.java.nano;
 import org.junit.Test;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 public class WebSocketChatApplicationTest {
@@ -20,7 +25,8 @@ public class WebSocketChatApplicationTest {
 
 
         // test login;
-        driver.get("localhost:8026");
+        String loginUrl = "localhost:8080";
+        driver.get(loginUrl);
         Assert.assertEquals(driver.getTitle(), "Chat Room Login");
         WebElement loginField = driver.findElement(By.name("username"));
 
@@ -34,8 +40,45 @@ public class WebSocketChatApplicationTest {
         System.out.println("Welcom: " + username.getText());
 
         // test send message;
+        WebElement msgSent = driver.findElement(By.id("msg"));
+        msgSent.sendKeys("Hello");
+        WebElement sendMsg = driver.findElement(By.id("sendMsgToServer"));
+        sendMsg.click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        List<WebElement> msgReceived = driver.findElements(By.className("message-content"));
+        Assert.assertEquals(msgReceived.get(0).getText(), "Helen: Hello");
+
+        // open a new window
+        WebDriver driver2 = new ChromeDriver();
+        driver2.get(loginUrl);
+        WebElement loginField2 = driver2.findElement(By.name("username"));
+
+        loginField2.sendKeys("Mary");
+        loginField2.submit();
+        WebElement username2 = driver2.findElement(By.id("username"));
+        WebElement userCount = driver2.findElement(By.className("chat-num"));
+        Assert.assertEquals(userCount.getText(), "2");
+
+        // test user2 send message
+        WebElement msgSent2 = driver2.findElement(By.id("msg"));
+        msgSent2.sendKeys("World");
+        WebElement sendMsg2 = driver2.findElement(By.id("sendMsgToServer"));
+        sendMsg2.click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        msgReceived = driver.findElements(By.className("message-content"));
+        Assert.assertEquals(msgReceived.size(), 2);
+        Assert.assertEquals(msgReceived.get(1).getText(), "Mary: World");
+
+        // test user leave
+        WebElement userExit = driver.findElement(By.id("exit"));
+        userExit.click();
+        userCount = driver2.findElement(By.className("chat-num"));
+        Assert.assertEquals(userCount.getText(), "1");
+
+
 
         // finish test
+        System.out.println("tests passed!");
         driver.quit();
     }
 }
